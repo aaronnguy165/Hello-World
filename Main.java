547d40cfa68f720767aa357446601fb97f95e888
aaronnguy165 <69646415+aaronnguy165@users.noreply.github.com>
2020-08-13 11:01:13 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println ("My name is Aaron");
+		System.out.println ("I like to hang out with friends");
+		System.out.println ("I want to go to UC university");
+		System.out.println ("I like APCS");
+	}
+}
